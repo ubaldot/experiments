0c7fe562ebe13dcac584385180c2d55621fbf13e
Ubaldo Tiberi <ubaldo.tiberi@volvo.com>
2024-06-03 12:12:06 +0200

    Added pipeline

--- test/run_tests.sh
+++ test/run_tests.sh
@@ -3,10 +3,19 @@
 # Script to run the unit-tests for the termdebug9.vim
 # Copied and adapted from Vim LSP plugin
 
+GITHUB=1
+
+# No arguments passed, then no exit
+if [ "$#" -eq 0 ]; then
+  GITHUB=0
+fi
+
 VIM_PRG=${VIM_PRG:=$(which vim)}
 if [ -z "$VIM_PRG" ]; then
   echo "ERROR: vim (\$VIM_PRG) is not found in PATH"
-  # exit 1
+  if [ "$GITHUB" -eq 1 ]; then
+	exit 1
+  fi
 fi
 
 VIM_CMD='$VIM_PRG -u NONE -U NONE -i NONE --noplugin -N --not-a-term'
@@ -22,14 +31,18 @@ RunTestsInFile() {
 
   if ! [ -f results.txt ]; then
     echo "ERROR: Test results file 'results.txt' is not found."
-    # exit 2
+	if [ "$GITHUB" -eq 1 ]; then
+	   exit 2
+	fi
   fi
 
   cat results.txt
 
   if grep -qw FAIL results.txt; then
     echo "ERROR: Some test(s) in $testfile failed."
-    # exit 3
+	if [ "$GITHUB" -eq 1 ]; then
+	  exit 3
+	fi
   fi
 
   echo "SUCCESS: All the tests in $testfile passed."
@@ -43,7 +56,9 @@ done
 
 echo "SUCCESS: All the tests passed."
 # UBA: uncomment the line below
-# exit 0
+if [ "$GITHUB" -eq 1 ]; then
+  exit 0
+fi
 # kill %- > /dev/null
 
 # vim: shiftwidth=2 softtabstop=2 noexpandtab
